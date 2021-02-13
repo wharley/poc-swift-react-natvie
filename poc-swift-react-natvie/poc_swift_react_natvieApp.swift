fb0f04f15c5b99c150b673d7ed32a308d7e71fe4
wharleyornelasjs <wharleyornelasjs@gmail.com>
2021-02-13 14:12:34 -0300

    Initial Commit

---
+++ poc-swift-react-natvie/poc_swift_react_natvieApp.swift
@@ -0,0 +1,17 @@
+//
+//  poc_swift_react_natvieApp.swift
+//  poc-swift-react-natvie
+//
+//  Created by Wharley Ornelas Rocha on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct poc_swift_react_natvieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
