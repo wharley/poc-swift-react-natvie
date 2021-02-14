a7f557b6b5101ba875454356e72f91fb1520669a
wharleyornelasjs <wharleyornelasjs@gmail.com>
2021-02-13 23:42:03 -0300

    Refactored folders.

---
+++ ios/poc-swift-react-natvie/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  poc-swift-react-natvie
+//
+//  Created by Wharley Ornelas Rocha on 2/13/21.
+//
+
+import SwiftUI
+
+struct Contacts: Identifiable {
+    let id: Int
+    let name: String
+    let course: String
+    let color: Color
+}
+
+struct ContentView: View {
+    var contactList = [
+        Contacts(id: 1, name: "Wharley Ornelas", course: "JavaScript", color: .red),
+        Contacts(id: 2, name: "João Rocha", course: "Java", color: .blue),
+        Contacts(id: 3, name: "Iara Rocha", course: "JavaScript", color: .red),
+        Contacts(id: 4, name: "Maria Rita Ornelas", course: "Banco de Dados", color: .green),
+        Contacts(id: 5, name: "Angelica Ornelas", course: "Java", color: .blue)
+    ]
+        
+    var body: some View {
+        NavigationView {
+            List(contactList) { contact in
+                HStack {
+                    Text(contact.name)
+                    Text(contact.course).foregroundColor(contact.color)
+                }
+            }.navigationTitle("Students")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/poc-swift-react-natvie/linktest-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  linktest-Bridging-Header.h
+//  poc-swift-react-natvie
+//
+//  Created by Wharley Ornelas Rocha on 2/13/21.
+//
+
+#ifndef linktest_Bridging_Header_h
+#define linktest_Bridging_Header_h
+#import "React/RCTBridgeModule.h"
+
+#endif /* linktest_Bridging_Header_h */
